2d851cb5363897f5f0f3fe07096e8ac698da20d4
Huy Ly <Huy.Ly@microsoft.com>
2020-10-22 16:59:49 +0000

    swapped score.py

--- code/deploy/score-simple.py
+++ code/deploy/score-simple.py
@@ -22,8 +22,8 @@ def init():
     model_file_name = "model.pkl"
     model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
     model = joblib.load(model_path)
-    inputs_dc = ModelDataCollector("sample-model", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
-    prediction_dc = ModelDataCollector("sample-model", designation="predictions", feature_names=["prediction"])
+    #inputs_dc = ModelDataCollector("sample-model", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
+    #prediction_dc = ModelDataCollector("sample-model", designation="predictions", feature_names=["prediction"])
 
 
 # The run() method is called each time a request is made to the scoring API.
@@ -32,13 +32,15 @@ def init():
 # run() method parses and validates the incoming payload against
 # the example input you provide here. This will also generate a Swagger
 # API document for your web service.
-@input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-@output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
+#@input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
+#@output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
 def run(data):
     # Use the model object loaded by init().
-    result = model.predict(data)
-    inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-    prediction_dc.collect(result) #this call is saving our input data into Azure Blob
+    print(type(data))
+    #result = model.predict(data)
+    #inputs_dc.collect(data) #this call is saving our input data into Azure Blob
+    #prediction_dc.collect(result) #this call is saving our input data into Azure Blob
 
     # You can return any JSON-serializable object.
-    return { "predict": result.tolist() }
+    #return { "predict": result.tolist() }
+    return "result = " + type(data)
